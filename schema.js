cda082a7c25b68b965936b17fe2a1451e40f9e56
Lam Nguyen <lam.nguyen.khmt2017@hcmut.edu.vn>
2020-04-12 11:11:56 +0700

    Setup Express and GraphQL Schema

---
+++ schema.js
@@ -0,0 +1,14 @@
+const {
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLInt,
+    GraphQLSchema,
+    GraphQLList,
+    GraphQLNonNull
+} = require('graphql');
+
+
+
+module.exports = new GraphQLSchema({
+
+});
